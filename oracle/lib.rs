a13b7356ddce54dfe67e46446cb304ebd53d9ef2
RastkoCBS <rastko@ceresblockchain.solutions>
2024-10-08 14:45:24 +0200

    Add tests for oracle contract

--- oracle/lib.rs
+++ oracle/lib.rs
@@ -15,6 +15,10 @@ mod oracle {
             Self { price }
         }
 
+        pub fn change_price(&mut self, new_price: Balance) {
+            self.price = new_price;
+        }
+
         #[ink(message)]
         pub fn get_price(&mut self) -> Balance{
             self.price
@@ -27,10 +31,21 @@ mod oracle {
         use super::*;
 
         #[ink::test]
-        fn it_works() {
+        fn create_oracle_works() {
             let oracle_price = 1000;
             let mut oracle = Oracle::new(oracle_price);
             assert_eq!(oracle.get_price(), oracle_price);
+        }
+
+        #[ink::test]
+        fn change_price_works() {
+            let oracle_price = 1000;
+            let new_price = 1200;
+
+            let mut oracle = Oracle::new(oracle_price);
+            
+            oracle.change_price(1200);
+            assert_eq!(oracle.get_price(), new_price);
         }
     }
 
